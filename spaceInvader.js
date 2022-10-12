f7b31221e4f526410b1fe80f3320fa7c93d2f823
nsongamampasi <113976889+nsongamampasi@users.noreply.github.com>
2022-10-12 13:38:32 +0200

    Add files via upload

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,40 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawline();
+
+function drawline() {
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+
+context.beginPath();
+context.fillStyle = "GREEN" ;
+context.rect(50,50,90,200);
+context.fill();
+
+context.beginPath();
+context.fillStyle = "GREEN" ;
+context.rect(100,50,400,90);
+context.fill();
+
+context.beginPath();
+context.fillStyle = "GREEN" ;
+context.rect(450,50,90,200);
+context.fill();
+
+context.beginPath();
+context.fillStyle = "GREEN" ;
+context.rect(140,250,310,310);
+context.fill();
+
+
+context.beginPath();
+context.fillStyle = "WHITE" ;
+context.rect(240,350,100,100);
+context.fill();
+   
+
+}
+
